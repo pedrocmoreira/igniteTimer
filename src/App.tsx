c68799c1404c8c5a82ea01caf87ccf8feb507ac3
Pedro Moreira <pedrocmoreira97@gmail.com>
2022-11-24 11:37:45 -0300

    Adicionando configurações para o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-export function App() {
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
+export function App() {
   return (
-    <div>
-      hello
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
