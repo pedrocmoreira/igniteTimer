5d42e15a1e178f8d6d78edfb4010dcf71f31190a
Pedro Moreira <pedro.henrique@agenciaalldigital.com>
2022-11-25 16:27:01 -0300

    Finalizando design das páginas home e history

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,11 +17,19 @@ export function Home() {
           <label htmlFor="">Vou trabalhar em</label>
           <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
 
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
+
           <label htmlFor="">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
           />
 
           <span>minutos.</span>
